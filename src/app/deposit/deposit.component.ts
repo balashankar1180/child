6779225af6191398b78e9426e18adf113f893867
balashankar1180 <balashankar1180@gmail.com>
2022-07-20 10:31:40 +0530

    gsg

--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 @Component({
-  selector: 'app-deposit',
-  template: `
-  <div>
-  <button (click)='coins()'>Deposit Coins</button>
-  <button (click)='notes()'>Deposit notes</button>
-  <p>
-  <router-outlet></router-outlet>
-  </p>
-</div>
-  `,
+selector: 'app-deposit',
+template: `
+<div>
+<button (click)='coins()'>Deposit Coins</button>
+<button (click)='notes()'>Deposit notes</button>
+<p>
+<router-outlet></router-outlet>
+</p>
+</div> `,
   styles: ['button {top: 50%; text-align: center;border-radius: 15px; border: 2px solid #73AD21; width: fit-content; margin: 20px; padding: 20px;}']
 })
 export class DepositComponent implements OnInit {
